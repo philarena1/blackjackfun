8f9d8b6c5dbf243711d160347e6fb9aeed5fbe0a
Philip Arena <philiplarena@gmail.com>
2019-08-15 20:23:31 -0400

    added class to create blackjack game

---
+++ hand.py
@@ -0,0 +1,57 @@
+# file for each hand played
+
+class Game():
+
+    def __init__(self):
+        # default game 1 player, 6 decks
+        Game.players = 1
+        Game.decks = 6
+        Game.deck_cards = [] # list of actual cards available
+        Game.shuffle_rate = 6 #times deck is shuffled
+
+
+
+    def test(self):
+        print('test')
+
+
+    def get_fresh_deck(self):
+        suits = ['H', 'S', 'C', 'D']
+        numbs = list(range(2, 11))
+
+        faces = ['J', 'Q', 'K', 'A']
+
+        deck = numbs + faces
+
+        full_deck = []
+        for suit in suits:
+            for card in deck:
+                full_deck.append(str(card) + suit)
+
+        num_decks = self.decks
+        total_decks = full_deck * num_decks
+
+        # dealer rules, hits on
+        from random import shuffle
+        shuffle_num = self.shuffle_rate  # how many times to shuffle
+
+        i = 0
+        while i < shuffle_num:
+            shuffle(total_decks)
+            i = i + 1
+
+        self.deck_cards = total_decks
+
+
+
+    def simulate(self, n):
+        """ n is the number of simulations to run"""
+
+
+
+
+new_game = Game()
+new_game.get_fresh_deck()
+
+
+
